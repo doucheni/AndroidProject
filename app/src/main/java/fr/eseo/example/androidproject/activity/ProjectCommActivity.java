51286dce3604d2eb3ce833845852bec2b2493aec
ines-jalouali <ines.jalouali@reseau.eseo.fr>
2020-10-07 14:51:33 +0200

    Project comm Activity

---
+++ app/src/main/java/fr/eseo/example/androidproject/activity/ProjectCommActivity.java
@@ -0,0 +1,9 @@
+package fr.eseo.example.androidproject.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProjectCommActivity extends AppCompatActivity {
+
+    public static final String PROJECT_ID = "Project_id";
+
+}
